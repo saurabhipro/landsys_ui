7a3b1c3d563c99a4fb56b0e1bc5933eb4b7c5dc2
sharry87 <sharvangkumar@gmail.com>
2022-09-10 19:15:31 +0530

    Login Header and View Encapsulation Removal

--- src/main/webapp/app/home/home.component.ts
+++ src/main/webapp/app/home/home.component.ts
@@ -11,7 +11,7 @@ import { HomeService } from './services/home.service';
   selector: 'jhi-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
-  encapsulation: ViewEncapsulation.None,
+  // encapsulation: ViewEncapsulation.None,
 })
 export class HomeComponent implements OnInit, OnDestroy {
   account: Account | null = null;
